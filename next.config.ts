dc613cdf077fd373d0dd8ba1163183d0fa97bd8a
Joshua Sosa Cruz <joshua.developer21@gmail.com>
2026-04-29 00:12:13 -0500

    Allow Supabase product images

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,15 @@ const nextConfig: NextConfig = {
       bodySizeLimit: "8mb",
     },
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
